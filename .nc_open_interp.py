9d7f1dc1fc71c8ead3a456a3719b109680a9a50f
dalianpanzi <96093100+dalianpanzi@users.noreply.github.com>
2024-04-09 23:18:27 +0800

    Update .nc_open_interp.py

--- .nc_open_interp.py
+++ .nc_open_interp.py
@@ -39,6 +39,19 @@ all_2020=Hebing(nc_2020)
 
 print(all_2017)#确定经纬度、时间范围
 
+all_2017.variables.keys() #查看变量
+
+‘’‘
+如果time是从X年X月X日 X：X：X的天数累计存储的，可以用.data直接把masked_array中的data数据读出。
+time = nc.num2date(all_2017.variables['time'][:],'days since 1800-1-1 00:00:00').data
+#输出：array([cftime.DatetimeGregorian(1891, 1, 1, 0, 0, 0, 0),
+cftime.DatetimeGregorian(1891, 2, 1, 0, 0, 0, 0),
+cftime.DatetimeGregorian(1891, 3, 1, 0, 0, 0, 0), …,
+cftime.DatetimeGregorian(2016, 10, 1, 0, 0, 0, 0),
+cftime.DatetimeGregorian(2016, 11, 1, 0, 0, 0, 0),
+cftime.DatetimeGregorian(2016, 12, 1, 0, 0, 0, 0)], dtype=object)
+‘’‘
+
 #选经纬度范围
 use_2017=all_2017.sel(latitude=slice(33,30),longitude=slice(122,124))
 use_2017['swh'][1].plot(x='longitude',y='latitude')
@@ -98,3 +111,43 @@ def process(array1,array2,list1,list2):
     return X_train,Y_train,X_test,Y_test
 
 X_train,Y_train,X_test,Y_test=process(x_tr,y_tr,x_te,y_te) 
+
+
+#BTW
+import dateutil.parser  #这个库来将日期字符串转成统一的datatime时间格式
+sdt = dateutil.parser.parse("2016-1-1T00:00:00")
+ #动态变化图
+import imageio
+frames = []  
+for i in range(12):  
+    frames.append(imageio.imread(str(i)+'.jpg'))  
+# Save them as frames into a gif   
+imageio.mimsave('动图.gif', frames, 'GIF', duration = 0.8) 
+
+#查找固定纬度/经度范围的，返回的是索引
+long= nc.variables['longitude'][:]  
+lati= nc.variables['latitude'][:] 
+print(np.argwhere(lati==4),np.argwhere(lati==54),
+np.argwhere(long==73+180),np.argwhere(long==133+180))
+
+#绘图
+from mpl_toolkits.basemap import Basemap
+lon_0 = lons.mean()
+lat_0 = lats.mean()
+m = Basemap(lat_0=lat_0, lon_0=lon_0)
+# 绘制经纬线
+m.drawparallels(np.arange(-90., 91., 20.), labels=[1,0,0,0], fontsize=10)
+m.drawmeridians(np.arange(-180., 181., 40.), labels=[0,0,0,1], fontsize=10)
+# Add Coastlines, States, and Country Boundaries
+m.drawcoastlines()
+m.drawstates()
+m.drawcountries()
+
+# Add Colorbar
+cbar = m.colorbar(cs, location='bottom', pad="10%")
+cbar.set_label(tlml_units)
+
+# Add Title
+plt.title('Surface Air Temperature')
+plt.show()
+
